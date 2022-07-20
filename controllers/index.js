89abf558bb5956e7ecc7ee76e4e032cee007062c
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-20 17:23:55 -0500

    getAllPlants success

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,16 @@ const createPlant = async (req, res) => {
   }
 }
 
+const getAllPlants = async (req, res) => {
+  try {
+    const plants = await Plant.find()
+    return res.status(200).json({ plants })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  createPlant
+  createPlant,
+  getAllPlants
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
 
+router.get('/plants', controllers.getAllPlants)
+
 router.post('/plants', controllers.createPlant)
 
 module.exports = router
