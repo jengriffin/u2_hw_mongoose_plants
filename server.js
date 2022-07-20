c7229a18e883527bd7af2ef8c4372f89ffd0bafd
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-20 17:43:53 -0500

    morgon install success

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
+const logger = require('morgan')
 
 // require() imports and middleware here ^ ///////
 
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json())
-
+app.use(logger('dev'))
 // app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes)
